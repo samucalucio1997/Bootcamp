ecef881ffd282280ac0252f897a020bec85a8aeb
Samuel Lúcio <102866875+samucalucio1997@users.noreply.github.com>
2023-08-17 11:34:36 -0300

    Update ContaTerminal.java

--- BancoTerminal/src/ContaTerminal.java
+++ BancoTerminal/src/ContaTerminal.java
@@ -4,11 +4,10 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Scanner entrada = new Scanner(System.in);
         
-        System.out.println("Informe seu nome"); 
-        String nomeCliente = entrada.nextLine();
 
         System.out.println("Por favor, digite o número da agência!");
         int NumeroConta = entrada.nextInt();
+    
         
         System.out.println("Digite o número de agência!");
         String Agencia = entrada.next();
@@ -17,6 +16,8 @@ public class ContaTerminal {
         System.out.println("Informe o valor a ser depositado!");
         double saldo = entrada.nextDouble();
         entrada.nextLine(); 
+        System.out.println("Informe seu nome"); 
+        String nomeCliente = entrada.nextLine(); 
         
         ContaBanco conta = new ContaBanco();
         conta.CriarConta(NumeroConta, Agencia, nomeCliente, saldo);
